22656a7e5c737eb8cc5f256a358ce67cb602c83b
Adam Caprez <acaprez2@unl.edu>
2017-06-23 16:39:50 +0000

    Add workaround for Docker images where /usr/{bin,sbin,lib,lib64} is RO.

--- libexec/image-handler.sh
+++ libexec/image-handler.sh
@@ -95,6 +95,8 @@ case "$SINGULARITY_IMAGE" in
                     chmod u+rwx "$SINGULARITY_ROOTFS/${file%/}"
                 fi
             done
+            # Workaround for images where /usr/{bin,sbin,lib,lib64} is mounted read-only
+            chmod -f u+rw $SINGULARITY_ROOTFS/usr/{bin,sbin,lib,lib64}
         done
 
         rm -f "$SINGULARITY_CONTENTS"
